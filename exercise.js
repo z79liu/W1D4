6b5f3cababfd2d4a09fd718ebdda24954a20a9a6
Zeyu Liu <haozai_03@hotmail.com>
2019-03-07 16:06:22 +0000

    print when waldo is found

---
+++ exercise.js
@@ -0,0 +1,14 @@
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+      console.log('Waldo is at ' + 'index ' + i)
+    }
+  }
+}
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
