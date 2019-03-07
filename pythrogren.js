efe12e62a0eec94497ba01e8572e64ead5c72096
Zeyu Liu <haozai_03@hotmail.com>
2019-03-07 18:07:54 +0000

    problem solved

---
+++ pythrogren.js
@@ -0,0 +1,17 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+
+var result = input.map(num => {
+  return Math.sqrt(Math.pow(num.x, 2) + Math.pow(num.y, 2))
+})
+
+ console.log(result)
+
+
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
