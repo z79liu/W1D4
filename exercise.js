5cf4edd852d2d61a3aa6f2039fbdf0dd071d4720
Zeyu Liu <haozai_03@hotmail.com>
2019-03-07 16:27:08 +0000

    finding waldo through callback

--- exercise.js
+++ exercise.js
@@ -1,8 +1,8 @@
+
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
-
     }
   }
 }
@@ -10,7 +10,6 @@ function findWaldo(arr, found) {
 function actionWhenFound(i) {
   console.log("Found him!");
   console.log('Waldo is at ' + 'index ' + i)
-
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
