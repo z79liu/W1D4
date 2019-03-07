25785bb4eb7d39a9f0aa733dbd052e40030bc8cf
Zeyu Liu <haozai_03@hotmail.com>
2019-03-07 17:13:11 +0000

    found waldo using ForEach

--- exercise.js
+++ exercise.js
@@ -1,4 +1,4 @@
-
+// find waldon using call back
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
@@ -10,6 +10,29 @@ function findWaldo(arr, found) {
 function actionWhenFound(i) {
   console.log("Found him!");
   console.log('Waldo is at ' + 'index ' + i)
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+// find waldo using for each loop
+
+function findWaldo(arr, found) {
+    arr.forEach(function(item){
+      if (found(item)) {
+        console.log('waldo is at ' + arr.indexOf(found(item)))
+      }
+})
+}
+
+function actionWhenFound(i) {
+  if (i === "Waldo") {
+    return i }
+    else {
+      return false
+    }
+        ;   // execute callback
+
+
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
